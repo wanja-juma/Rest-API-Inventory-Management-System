d980d23a8c69af7efbeaea3c7fa126a94f8ec4d3
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-07-06 03:17:39 +0300

    Implement GET inventory endpoints

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-
 from flask import Flask, jsonify, request
 
 from data import inventory
-
+from helpers import (
+    fetch_product_by_barcode,
+    fetch_product_by_name
+)
 
 app = Flask(__name__)
 
@@ -18,3 +20,24 @@ def home():
     return {
         "message": "Inventory Management REST API"
     }
+
+# GET ALL INVENTORY
+
+@app.route("/inventory", methods=["GET"])
+def get_inventory():
+    return jsonify(inventory), 200
+
+# GET SINGLE ITEM
+
+@app.route("/inventory/<int:item_id>", methods=["GET"])
+def get_item(item_id):
+
+    item = next(
+        (i for i in inventory if i["id"] == item_id),
+        None
+    )
+
+    if item is None:
+        return jsonify({"error": "Item not found"}), 404
+
+    return jsonify(item), 200
