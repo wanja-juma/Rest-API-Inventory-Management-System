3689991dea8bdfbc125014d921606b1125fd813c
ruthjuma100@gmail.com <ruthjuma100@gmail.com>
2026-07-06 03:53:55 +0300

    unit tests for post and mssing fields

--- tests/test_api.py
+++ tests/test_api.py
@@ -21,3 +21,54 @@ def test_get_single_item(client):
 
     assert product["id"] == 1
     assert product["product_name"] == "Nutella"
+
+# GET INVALID ITEM
+
+def test_invalid_item(client):
+
+    response = client.get("/inventory/999")
+
+    assert response.status_code == 404
+    assert response.get_json()["error"] == "Item not found"
+
+# POST
+
+@patch("app.fetch_product_by_barcode")
+def test_add_item(mock_api, client):
+
+    mock_api.return_value = {
+        "product_name": "Fanta",
+        "brand": "Coca Cola",
+        "ingredients": "Water, Sugar"
+    }
+
+    response = client.post(
+        "/inventory",
+        json={
+            "barcode": "987654321",
+            "price": 4.99,
+            "stock": 40
+        }
+    )
+
+    assert response.status_code == 201
+
+    product = response.get_json()
+
+    assert product["product_name"] == "Fanta"
+    assert product["price"] == 4.99
+    assert product["stock"] == 40
+
+# POST MISSING FIELDS
+
+def test_post_missing_fields(client):
+
+    response = client.post(
+        "/inventory",
+        json={
+            "barcode": "11111"
+        }
+    )
+
+    assert response.status_code == 400
+    assert response.get_json()["error"] == "Missing required fields"
